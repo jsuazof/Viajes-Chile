577bac71c30f8a2dbbfabfaeb386123ef90eb8e5
Jorge <jsuazof@gmail.com>
2022-01-02 14:42:03 -0300

    Documentacion indentacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
 $(function () {   //Esta es la función general de jquery
     //Todos los métodos de jquery van acá adentro
+
     //Oculta informacion al hacer click en el titulo de las tarjetas
     $('.card-title').click(function () {
         $('.card-text2').toggle();
     });
+
+    //Tooltips aplicado en la seccion Quienes Somos
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
