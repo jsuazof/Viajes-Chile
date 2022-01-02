3e141be56143aa21ce2cfc93873c20c9774f05d1
Jorge <jsuazof@gmail.com>
2022-01-01 21:26:32 -0300

    Chequeo Sintaxis

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(function () {   //Esta es la función general de jquery
     //Todos los métodos de jquery van acá adentro
     $('.card-title').click(function () {
-        $('.txt-2').toggle();
+        $('.card-text2').toggle();
     });
 
 });
