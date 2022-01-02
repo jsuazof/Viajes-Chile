9531c841088f3e18f203183baca0c181493a2ca2
Jorge <jsuazof@gmail.com>
2022-01-02 14:35:21 -0300

    Tooltip seccion quienes somos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
 $(function () {   //Esta es la función general de jquery
-                  //Todos los métodos de jquery van acá adentro
-//Oculta informacion al hacer click en el titulo de las tarjetas
+    //Todos los métodos de jquery van acá adentro
+    //Oculta informacion al hacer click en el titulo de las tarjetas
     $('.card-title').click(function () {
         $('.card-text2').toggle();
     });
-
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 });
